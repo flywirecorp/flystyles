ae8bf341b9767dd3ac8cd2a747fd0cc63506fd2f
Javi Arques <javier.arques@flywire.com>
2016-07-04 12:30:07 +0200

    add Flystyle class with helpers

--- lib/flystyles.rb
+++ lib/flystyles.rb
@@ -1,2 +1,32 @@
 require "flystyles/version"
 require "flystyles/engine"
+
+module Flystyles
+  class << self
+
+    # Paths
+    def gem_path
+      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
+    end
+
+    def stylesheets_path
+      File.join assets_path, 'scss'
+    end
+
+    def fonts_path
+      File.join assets_path, 'fonts'
+    end
+
+    def javascripts_path
+      File.join assets_path, 'js'
+    end
+
+    def components_path
+      File.join assets_path, 'components'
+    end
+
+    def assets_path
+      @assets_path ||= File.join gem_path, 'assets'
+    end
+  end
+end
