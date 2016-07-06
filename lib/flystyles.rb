3cb13d3e09c58d25b4ba1a87cf82aabd8491bda2
Javi Arques <javier.arques@flywire.com>
2016-07-06 15:57:09 +0200

    adding render_component view helper

--- lib/flystyles.rb
+++ lib/flystyles.rb
@@ -1,5 +1,6 @@
 module Flystyles
+  require 'flystyles/version'
   require 'flystyles/engine'
   require 'flystyles/railtie'
-  require 'flystyles/version'
+  require 'flystyles/view_helpers'
 end
--- lib/flystyles/railtie.rb
+++ lib/flystyles/railtie.rb
@@ -1,9 +1,7 @@
 module Flystyles
   class Railtie < Rails::Railtie
-    initializer 'flystyles.add_view_paths', :after => :add_view_paths do |app|
-      ActiveSupport.on_load(:action_controller) do
-        prepend_view_path app.root.join('/home/vagrant/code/flystyles/src/components').to_s
-      end
+    initializer "flystyles.view_helpers" do
+      ActionView::Base.send :include, ViewHelpers
     end
   end
 end
