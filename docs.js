81516d98d88ed0813f09fc894111c776eec2c621
Javi Arques <javier.arques@flywire.com>
2016-11-21 15:28:13 +0100

    Change docs.js script to generate build files before starting browserSync

--- docs.js
+++ docs.js
@@ -1,54 +1,56 @@
 // Create a Browsersync instance
-const bs = require("browser-sync").create();
+const bs = require('browser-sync').create();
 const exec = require('child_process').exec;
+const buildPath = './_site';
 
+// exec jekyll build for the first time
+exec('bundle exec jekyll build', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`${error}`);
+    return;
+  }
+  console.log(`${stdout}`);
+  console.log(`${stderr}`);
+
+  // Now init the Browsersync server
+  bs.init({
+    server: buildPath
+  });
+});
 
 // Provide a callback to capture ALL events to CSS
 // files - then filter for 'change' and reload all
 // css files on the page.
-bs.watch("./**/*.scss", function(event, file) {
+bs.watch('./**/*.scss', (event, file) => {
 
-  if (event === "change") {
-    //bs.reload("*.css");
-    var build = exec('bundle exec jekyll build');
+  if (event === 'change') {
+    //bs.reload('*.css');
+    const build = exec('bundle exec jekyll build');
 
-    build.stdout.on('data', function(data) {
+    build.stdout.on('data', (data) => {
       console.log(data);
-      bs.reload("*.css");
+      bs.reload('*.css');
     });
 
-    build.stderr.on('data', function(data) {
-      console.log(data);
-    });
+    build.stderr.on('data', (data) => console.log(data));
 
-    build.on('close', function(code) {
-      console.log(code);
-    });
+    build.on('close', (code) => console.log(code));
   }
 });
 
 // Provide a callback to capture ALL events to HTML
-bs.watch(["./docs/**/*.html", "./docs/**/*.md"], function(event, file) {
+bs.watch(['./docs/**/*.md'], (event, file) => {
 
-  if (event === "change") {
-    var build = exec('bundle exec jekyll build');
+  if (event === 'change') {
+    const build = exec('bundle exec jekyll build');
 
-    build.stdout.on('data', function(data) {
+    build.stdout.on('data', (data) => {
       console.log(data);
       bs.reload();
     });
 
-    build.stderr.on('data', function(data) {
-      console.log(data);
-    });
+    build.stderr.on('data', (data) => console.log(data));
 
-    build.on('close', function(code) {
-      console.log(code);
-    });
+    build.on('close', (code) => console.log(code));
   }
-});
-
-// Now init the Browsersync server
-bs.init({
-  server: './_site'
 });
