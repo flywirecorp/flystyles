10f79d996b86bda1771d0559488eede66debb01f
Vicent Gisbert <vicgisso@gmail.com>
2022-01-13 08:49:52 +0100

    delete old jekyll structure for documentation

--- docs_old/javascript/menu.js
+++
@@ -1,6 +0,0 @@
-document
-  .querySelector('[data-js="mobile-menu"]')
-  .addEventListener("change", function(e) {
-    console.log(e.target.value);
-    window.location.href = e.target.value;
-  });
